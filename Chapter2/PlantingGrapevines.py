ef66e41377f4fb0d66897a37f5c4a9780348a87e
SimplyClueless <benjaminbristowrocks@yahoo.com>
2020-02-14 12:18:10 +1000

    14/2/2020

---
+++ Chapter2/PlantingGrapevines.py
@@ -0,0 +1,6 @@
+R = float(input("Enter the length of a row in 'feet': "))
+E = float(input("Enter the amount of space for end post assembly in 'feet': "))
+S = float(input("Enter the space between the vines in 'feet': "))
+V = (R - 2*E) / S
+
+print("The number of grapvines is",V)
