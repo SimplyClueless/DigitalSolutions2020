745ff98adf626c4453e435e8f91a9d6e4b4d909b
SimplyClueless <benjaminbristowrocks@yahoo.com>
2020-02-26 09:03:10 +1000

    25/2/2020 If Statement Work

--- Chapter3/IfElseStatements7.py
+++ Chapter3/IfElseStatements7.py
@@ -6,3 +6,9 @@ HPackets = total // 10
 HRemainder = total % 10
 BPackets = total // 8
 BRemainder = total % 8
+
+print("Total =", total)
+print("Hotdog packets =", HPackets)
+print("Remaining Hotdogs =", HRemainder)
+print("Bun pakcets =", BPackets)
+print("Remaining Buns =", BRemainder )
