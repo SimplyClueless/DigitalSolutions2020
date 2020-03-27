c5e449abcb43bfdecef79a0c43e86e2212e5f791
SimplyClueless <benjaminbristowrocks@yahoo.com>
2020-03-27 12:30:48 +1000

    27/3/2020 guiZero

--- Classwork/MyGUI.py
+++ Classwork/MyGUI.py
@@ -1,17 +1,15 @@
-from guizero import App, Text, Picture, PushButton, MenuBar
+from guizero import App, Text, Picture, PushButton, MenuBar, Slider, TextBox, CheckBox, Combo
 
 app = App()
-
-text = Text(app, align="top", text="Hello GUI", color="blue", font="Times New Roman", size="30")
-text1 = Text(app, align="bottom", text="General Kenobi")
-
-picture = Picture(app, image="picture1.png", width=100, height=100)
-picture1 = Picture(app, image="picture2.png", width=100, height=100)
-
 
 def DoSomething():
     print("You have pushed my button")
 
+def SliderChanged(value):
+    textbox.value = value
+
+def CheckBoxChanger():
+    textbox1 = TextBox(app, text="Gays Burnt!")
 
 menubar = MenuBar(app,
                   toplevel=["File", "Edit", "Settings"],
@@ -20,6 +18,18 @@ menubar = MenuBar(app,
                       [["Edit option 1", DoSomething], ["Edit option 2", DoSomething]],
                       [["Settings option 1", DoSomething], ["Settings option 1", DoSomething]]
                   ])
+
+textbox = TextBox(app, align="bottom")
+
+text = Text(app, align="top", text="Hello GUI", color="blue", font="Times New Roman", size="30")
+text1 = Text(app, align="bottom", text="General Kenobi")
+
+picture = Picture(app, image="picture1.png", width=300, height=300)
+picture1 = Picture(app, image="picture2.png", width=300, height=300)
+
+slider = Slider(app, align="bottom", command=SliderChanged)
+
+checkbox = CheckBox(app, text="Burn the Gays!", command=CheckBoxChanger)
 
 button = PushButton(app, text="Push me!", command=DoSomething)
 button.bg = "green"
