b79bf4c9fd3166f2a957d880fcd3c557dab9c138
SimplyClueless <benjaminbristowrocks@yahoo.com>
2020-04-27 09:14:53 +1000

    27/4/2020 chapter 5

--- Chapter5/NumberGuessingGame.py
+++ Chapter5/NumberGuessingGame.py
@@ -1,3 +1,14 @@
-CorrectNumber = false
+import random
 
-while Correct != true
+RandomNumber = random.randint(1, 101)
+CorrectNumber = False
+
+while CorrectNumber != True:
+    UserNumber = int(input("I have picked a number between 1 and 100, guess what it is: "))
+    if UserNumber > RandomNumber:
+        print("That number is too high, try again!")
+    if UserNumber < RandomNumber:
+        print("That number is too low, try again!")
+    if UserNumber == RandomNumber:
+        print("Congratulations! You guessed the number correct!")
+        CorrectNumber = True
