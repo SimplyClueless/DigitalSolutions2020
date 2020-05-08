76fd4c954996c52d4f4389e86c1be811e0787a5a
SimplyClueless <39248934+SimplyClueless@users.noreply.github.com>
2020-05-08 11:50:51 +1000

    8/5/2020 Login Page

---
+++ Classwork/Login.py
@@ -0,0 +1,24 @@
+from guizero import App, Window, TextBox, Text, PushButton
+
+app = App(title="Login Screen", layout="grid")
+window = Window(app, title="GUI", layout="grid")
+window.hide()
+
+Username = "bristow"
+Password = "password"
+
+UsernameText = TextBox(app, grid=[0, 0], width="50")
+PasswordText = TextBox(app, grid=[0, 1], width="50", hide_text=True)
+
+def CheckCredentials():
+    text = Text(app, grid=[0, 2])
+    text.clear()
+    if UsernameText.value == Username and PasswordText.value == Password:
+        app.hide()
+        window.show()
+    else:
+        text = Text(app, text="Incorrect Credentials", grid=[0, 3])
+
+
+SubmitLoginDetails = PushButton(app, text="Login", grid=[0, 4], command=CheckCredentials)
+app.display()
