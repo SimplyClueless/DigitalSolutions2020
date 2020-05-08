db7a1aa2f2c0fe2104ee43e2b0441d4ae65d4737
SimplyClueless <39248934+SimplyClueless@users.noreply.github.com>
2020-05-08 12:18:15 +1000

    8/5/2020 Login Page and List Practice

--- Classwork/Login.py
+++ Classwork/Login.py
@@ -8,7 +8,7 @@ Username = "bristow"
 Password = "password"
 
 UsernameText = TextBox(app, grid=[0, 0], width="50", align="top")
-PasswordText = TextBox(app, grid=[0, 1], width="50", align="top", align hide_text=True)
+PasswordText = TextBox(app, grid=[0, 1], width="50", align="top", hide_text=True)
 
 def CheckCredentials():
     text = Text(app, grid=[0, 2])
@@ -18,7 +18,6 @@ def CheckCredentials():
         window.show()
     else:
         text = Text(app, text="Incorrect Credentials", grid=[0, 3], align="top")
-
 
 SubmitLoginDetails = PushButton(app, text="Login", grid=[0, 4], align="top", command=CheckCredentials)
 app.display()
