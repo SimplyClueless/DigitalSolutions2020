8a68324ece5128fb563ae719a302623299a49b81
SimplyClueless <benjaminbristowrocks@yahoo.com>
2020-08-28 12:09:45 +1000

    28/08/2020

--- Classwork/testProjects.py
+++ Classwork/testProjects.py
@@ -1,19 +1,7 @@
-import statistics
-
-stats = statistics
-
-numbers = []
-continueInput = "y"
-
-while continueInput == "y" or continueInput == "Y":
-    numberInput = int(input("Enter a number: "))
-    numbers.append(numberInput)
-
-    continueInput = str(input("Continue adding numbers: "))
-
-numbers.sort()
-print(numbers)
-print("Length is:", len(numbers))
-print("Mean is:", stats.mean(numbers))
-print("Median is:", stats.median(numbers))
-print("Mode is:", stats.mode(numbers))
+while True:
+    try:
+        number = int(input("Enter a number: "))
+    except:
+        print("Invalid Variable!")
+    else:
+        print(number)
